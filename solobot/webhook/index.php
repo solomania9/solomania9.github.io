b8a74259f9e6faa6590b2c6d4c527a130cf78545
solomania9 <mike@solomania.com>
2016-05-27 10:43:48 -0400

    added solobot

---
+++ solobot/webhook/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Solobot</title>
+
+  </head>
+  <body>
+
+hello - this is the webbook section.
+
+
+  </body>
+</html>
